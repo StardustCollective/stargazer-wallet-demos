b05c958bb168748d19979058f8c9e8965049befa
Juan David Suarez <juandavidkincaid@gmail.com>
2022-06-01 15:25:29 -0500

    renamed demo

--- demos/web3-react/src/common/consts/connectors.ts
+++ demos/web3-react/src/common/consts/connectors.ts
@@ -1,7 +1,15 @@
 import {StargazerConnector} from 'stargazer-connector';
 
+declare global {
+  interface Window {
+    stargazerConnector: StargazerConnector;
+  }
+}
+
 const stargazerConnector = new StargazerConnector({
   supportedChainIds: [1, 3]
 });
+
+window.stargazerConnector = stargazerConnector;
 
 export {stargazerConnector};
